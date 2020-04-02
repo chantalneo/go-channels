6f0e6e5b704548e34aea7334dac8e720a9f49a1d
Chantal Neo <Chantal@hotmail.sg>
2020-04-02 22:25:46 +0800

    Go Routines

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ func main() {
 }
 
 func checkLink(link string) {
-	_, err := http.Get(link)
+	_, err := http.Get(link) // Blocking call. When this runs, Main Go routine can do nothing else!
 	if err != nil {
 		fmt.Println(link, "might be down!")
 		return
@@ -37,3 +37,12 @@ func checkLink(link string) {
 //
 //    Basically it's in series, every single time we're making a request, We sit around and wait for the response to come back before making the next
 //    So our aim to to make requests in parallel
+//
+// 2. When we launch a Go program, i.e. when we compile it and execute it, we automatically create one Go routine. You can think of a routine as being
+//    something that exists inside of our running program or our process. This go routine takes every line of code inside of our program and executes them one by one.
+//    Actual compiled form of our code might look a little bit differently than what we have.
+//
+// 3. Syntax of a Go routine:
+//    go checkLink(link)
+//    go - creates a new thread go routine
+//    checkLink(link) - the function the newly created thread would run
